cfaebcf0be5364217028bcde070b9feb36a02fcb
AMINE294961 <bouttemohamedamin@gmail.com>
2026-08-20 04:15:12 +0100

    Redirect home to login

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\CommentController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
